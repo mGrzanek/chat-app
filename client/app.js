cf0233eb94d45836433bbb7d7ce057f6a22f2e66
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-26 11:50:51 +0100

    Add sendMessage and addMessage  functions, add listener on addMessageForm element, update message section in html code

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@ const userNameInput = loginForm.querySelector('[id="username"]');
 const messagesSection = document.getElementById('messages-section');
 const messagesList = messagesSection.querySelector('.messages-section__list');
 const addMessageForm  = document.getElementById('add-messages-form');
+const messageContentInput = addMessageForm.querySelector('#message-content');
 let userName;
 
 const login = e => {
@@ -14,6 +15,30 @@ const login = e => {
     } else alert('Username required!');
 };
 
+const addMessage = (author, content) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+        <h3 class="message__author">${(author === userName ? 'You' : author)}</h3>
+        <div class="message__content">${content}</div>
+    `;
+    messagesList.appendChild(message);
+};
+
+const sendMessage = e => {
+    e.preventDefault();
+    if(messageContentInput.value){
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = '';
+    } else alert('Empty message!');
+};
+
 loginForm.addEventListener('submit', function(e){
     login(e);
+});
+
+addMessageForm.addEventListener('submit', function(e) {
+    sendMessage(e);
 });

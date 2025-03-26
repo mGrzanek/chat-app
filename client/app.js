0c832504c941db7f037f5094eba844bf20133741
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-26 11:47:55 +0100

    Add login function and add listener on loginForm element

--- client/app.js
+++ client/app.js
@@ -4,3 +4,16 @@ const messagesSection = document.getElementById('messages-section');
 const messagesList = messagesSection.querySelector('.messages-section__list');
 const addMessageForm  = document.getElementById('add-messages-form');
 let userName;
+
+const login = e => {
+    e.preventDefault();
+    if(userNameInput.value){
+        userName = userNameInput.ariaValueMax;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    } else alert('Username required!');
+};
+
+loginForm.addEventListener('submit', function(e){
+    login(e);
+});

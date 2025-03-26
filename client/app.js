aad1d96f9bc7fb43f709446266cd56e4609c5847
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-26 11:44:48 +0100

    Add references to html elements

---
+++ client/app.js
@@ -0,0 +1,6 @@
+const loginForm = document.getElementById('welcome-form');
+const userNameInput = loginForm.querySelector('[id="username"]');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = messagesSection.querySelector('.messages-section__list');
+const addMessageForm  = document.getElementById('add-messages-form');
+let userName;
